e48746df80d0df0fbbdd88ba1f1e784844f719f6
Jon Dodd <jon@dodd.xyz>
2016-01-19 21:45:33 +0000

    Product images

--- src/js/origin.js
+++ src/js/origin.js
@@ -1,15 +1,29 @@
 
 $(document).ready(function(){
-	
+
 	// Mobile nav
 	$('.nav__toggle').click(function(event) {
 		event.preventDefault();
 		$(this).toggleClass('nav__toggle--active');
 		$('.nav__items').toggleClass('nav__items--show');
 	});
-	if ($('html').hasClass('no-touchevents')) {
+	if ($('html').hasClass('touchevents')) {
 		$('.nav__parent>a').one( "click", function(event) {
-			event.preventDefault(); 
+			event.preventDefault();
 		});
 	}
+
+	// Image toggle
+	$('.product-main__image img:first-child').addClass('toggle-element--open');
+	$('.toggle-element').click(function(event) {
+		event.preventDefault();
+
+		var toggle_id = $(this).attr('data-toggle');
+
+		$(this).toggleClass('toggle-element--active');
+		$('.product-main__image img').removeClass('toggle-element--open');
+		$("#"+toggle_id).toggleClass('toggle-element--open');
+		$('body').toggleClass(toggle_id+'--open');
+	});
+
 });
